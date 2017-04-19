22fb7fb184f26d7417bec730023d7633d2b74b88
Guillermo Loaysa <gloaysa@gmail.com>
2017-04-19 20:32:29 +0200

    Commiting changes just for the sake of history

--- schematizer/ph1w1b.js
+++ schematizer/ph1w1b.js
@@ -28,30 +28,63 @@
 // ---------------------------------------------------------------------------
 
 //---- we start with our new Object example with some default values on it
+// note that we need every key of the object being an object itself, so we can
+// store default values on them
 
-var newSchema = {
-  name: "description",
-  arg: 0,
-  operation: function(){
-    return "this is the default function!";
+var example = {
+  name: {
+    is: "string", //what the user should prompt?
+    value: "sum" //what is going to be it default opeation
+  },
+  arg: {
+    is: "number",
+    value: [1, 1]
+  },
+  operation: {
+    is: "function",
+    value: function(a, b) {return a + b;}
   }
 }
 
-//---- now the function that validates any schema passed
+//---- no we create an empty object to use later on
 
-var validation = function(name, arg, op){
-  // we store the values to use them later on
-  var array = [name, arg, op]
-  // we check if the arguments passed are correct
-  if (typeof(name) === typeof(newSchema.name) && typeof(arg) === typeof(newSchema.args) && typeof(op) === typeof(newSchema.operation)){
-    console.log("The operation " + array + " is correct"),
-    // create the object op with the function passed
-    console.log("The result of your function is " op() )
-  }
-// if they aren't correct, we pass the error
-  else {
-    if (typeof(name)!== typeof(newSchema.name)) { return "Your name isn't a string"}
-    else if (typeof(arg) !== typeof(newSchema.args)) { return arg + " is not a number"}
-    else { return op + " is not a function"};
+var newSchema = {}
+
+//---- Now, our Schema validation function
+// where op is going to be an object a match a function
+function validation(op){
+  for (name in op) {
+    if (typeof(op.name) === typeof(example.name.is)) {
+      newSchema.name = op.name
+    }
   }
 }
+
+
+
+// var newSchema = {
+//   name: "description",
+//   arg: 0,
+//   operation: function(){
+//     return "this is the default function!";
+//   }
+// }
+//
+// //---- now the function that validates any schema passed
+// function confirm(name, arg, op){
+//   // we store the values to use them later on
+//   var array = [name, arg, op]
+//   // we check if the arguments passed are correct
+//   // we could use typeof(name) === "string" but it this way if newSchema changes, it's still valid
+//   if (typeof(name) === typeof(example.name) && typeof(arg) === typeof(example.args[is]) && typeof(op) === typeof(example.operation.is)){
+//     console.log("The operation " + array + " is correct"),
+//     // we print to console the result of the user function
+//     console.log("The result of you function is " + op() )
+//   }
+// // if they aren't correct, we pass the error
+//   else {
+//     if (typeof(name)!== typeof(example.name)) { return "Your name isn't a string"}
+//     else if (typeof(arg) !== typeof(example.args[is])) { return arg + " is not a number"}
+//     else { return op + " is not a function"};
+//   }
+// }
