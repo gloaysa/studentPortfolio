0c9e4839dfad266c8879597b2f084ec509eb38f7
Guillermo Loaysa <gloaysa@gmail.com>
2017-04-24 14:07:56 +0200

    schemedCalc with addTo func and empty README

--- schemedCalc/ph1w2.js
+++ schemedCalc/ph1w2.js
@@ -19,115 +19,107 @@
 //       BEHAVIOR: resets 'this.schema' to the argument.  I can't imagine why this would fail.
 // ____________________________________________________
 
-// schemaCalc is an object (we're reusing the example object from ph1w1b)
+//|schemaCalc is an object containing a schema,         |
+//|operation objects and functions that add more        |
+//|operation objects, operate those objects             |
+//|and change them.                                     |
+//_______________________________________________________
+
 var schemaCalc = {
-  //   it contains an object with operation objects in it
-  sum: {
-    name: {type: "string", fallback: "sum up to 3 numbers"},
-    numArgs: {type: "number", fallback: [1, 2]},
-    operation: {type: "function", fallback: function(a, b, c = 0){
-      var result = a + b + c;
-      return result;
-    }
-  }},
-
-  sub: {
-    name: {type: "string", fallback: "subtract"},
-    numArgs: {type: "number", fallback: [2, 1]},
-    operation: {type: "function", fallback: function(a, b){
-      var result = a - b;
-      return result;
-    }
-  }},
-
-  mul: {
-    name: {type: "string", fallback: "Multiply up to 3 numbers"},
-    numArgs: {type: "number", fallback: [2, 2]},
-    operation: {type: "function", fallback: function(a, b, c = 1){
-      var result = a * b * c;
-      return result;
-    }
-  }},
-
-  div: {
-    name: {type: "string", fallback: "Divide 2 numbers"},
-    numArgs: {type: "number", fallback: [4, 2]},
-    operation: {type: "function", fallback: function(a, b){
-      var result = a / b;
-      return result;
-    }
-  }},
-
-  ave: {
-    name: {type: "string", fallback: "Calculate the average of up to 3 numbers"},
-    numArgs: {type: "number", fallback: [2, 3]},
-    operation: {type: "function", fallback: function(a, b, c = 0){
-      if (c === 0){ return (a + b)/2 }
-    }
-  }},
-  //an schema objects used to validate future operation objects
-  example: {
-    name: {type: "string", fallback: "I'm the default name"},
+  //an schema object used to validate future operation objects
+    name: {type: "string", fallback: "schema"},
     numArgs: {type: "number", fallback: 1},
     operation: {type: "function", fallback: function(){
         console.log("I'm the default operation")
-      }
-    }},
+    }
+  }
+};
+  //operation array to store the operations
+var operation = [];
+
+    //**FUNCTIONS**
+
+//|addTo is a function that match new objects against   |
+//|the schema & add them to operations if they're valid |
+//|returning a success/failure msg.                     |
+//_______________________________________________________
+
+function addTo(newObj){
+  //the method 'addTo' uses schema objects to validate new objects
+  for (key in newObj){
+    if (key in schemaCalc){
+      if (key.type === schemaCalc.key)
+      console.log("Your Object has been added to operations");
+      operation.push(newObj);
+      return operation
+    }else {
+      console.log("Your object didn't match the schema");
+      return undefined
+    }
+  }
+};
 
     //a method 'operate' that searches for an operation with the name passed in
     //contains a string indicating operation to be used and numbers to be operated on
-    operate: function(op, a, b, c){
-      //if success, returns an array containing success msg and result of operation
-      if (op in this) {
-        operation = this[op];
-        console.log("The operation", op, "was a success!")
-        return operation.operation.fallback(a, b, c);
+function operate(op, a, b, c){
+  //if success, returns an array containing success msg and result of operation
+  for (i = 0; i < operation.length; i++){
+    console.log("looooooooping!");
+    for (key in op){
+      if (key in operation[i]) {
+        if (operation.fallback(a, b, c != NaN)){
+          console.log("The operation", op, "was a success!")
+          return operation.fallback(a, b, c);
+        }else { console.log("Please, pass numbers"); return undefined
+        }
       }//else, msg telling operation was a failure, return undefined
       else {
         console.log("Operation", op,  "hasn't been found :(");
         return undefined
       }
-
     }
-}
-
-//We'll need an 'operation' array to store the valid objects passed to 'addTo()'
-operation = []
-
-//the method 'addTo' uses schema objects to validate new objects
-function schematizer(schema, op){
-  for (key in op) {
-    console.log("Key is ", key, " and op[key].type is ", op[key].type);
-  	var llave = op[key];
-    if (key in schema){
-      console.log(key, "is in the ", schema)
-      if (llave.type === schema[key].type) {
-        myObject[key] = op[key];
-        addToCollection = true;
-      } else {
-        myObject[key] = schema[key]
-        console.log(llave.type, "isn't in the schema. Default value added".toUpperCase());
-      }
-    } else {
-        addToCollection = false;
-        myObject = {};
-        console.log(key, " isn't in schema".toUpperCase());
-        return op;
-     }
   }
-  if ('name' in op){ //Challenge: property required ('name').
-    if (addToCollection === true){ //Challenge: remember is an object was valid.
-      myCollection.push(myObject); // We made it!!! :)
-      console.log("New object added to myCollection ".toUpperCase(), myCollection);
-      return myObject
-    } else { // if it wasn't valid, just return the Object passed.
-      return op
-    }
-  } // If name doesn't exist, it returns the Object passed
-    myObject = {};
-    console.log("Property 'name' is required".toUpperCase());
-    return op
-}
+};
+    //test scripts:
+    //schemaCalc.operate("sum", 2, 3, 1); -- 6
+    //schemaCalc.operate("sum"); -- undefined
+    //schemaCalc.operate("assets", 2, 1); -- undefined
+
+
+
+// function schematizer(schema, op){
+//   for (key in op) {
+//     console.log("Key is ", key, " and op[key].type is ", op[key].type);
+//   	var llave = op[key];
+//     if (key in schema){
+//       console.log(key, "is in the ", schema)
+//       if (llave.type === schema[key].type) {
+//         myObject[key] = op[key];
+//         addToCollection = true;
+//       } else {
+//         myObject[key] = schema[key]
+//         console.log(llave.type, "isn't in the schema. Default value added".toUpperCase());
+//       }
+//     } else {
+//         addToCollection = false;
+//         myObject = {};
+//         console.log(key, " isn't in schema".toUpperCase());
+//         return op;
+//      }
+//   }
+//   if ('name' in op){ //Challenge: property required ('name').
+//     if (addToCollection === true){ //Challenge: remember is an object was valid.
+//       myCollection.push(myObject); // We made it!!! :)
+//       console.log("New object added to myCollection ".toUpperCase(), myCollection);
+//       return myObject
+//     } else { // if it wasn't valid, just return the Object passed.
+//       return op
+//     }
+//   } // If name doesn't exist, it returns the Object passed
+//     myObject = {};
+//     console.log("Property 'name' is required".toUpperCase());
+//     return op
+// }
 //     it returns a success or failure message
 //
 
