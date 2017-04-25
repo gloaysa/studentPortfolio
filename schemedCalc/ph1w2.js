ce7fdd10d634b686a58751babca4200e0428b59c
Guillermo Loaysa <gloaysa@gmail.com>
2017-04-26 01:01:49 +0200

    schemaCalc finished - fixed some typos

--- schemedCalc/ph1w2.js
+++ schemedCalc/ph1w2.js
@@ -1,35 +1,38 @@
+//|________________________________________________|
 //| schemaCalc is an object containing a schema,   |
 //| operation objects and functions that add more  |
 //| operation objects, operate those objects       |
-//| and change them.                               |
+//| and change the schema.                         |
 //|________________________________________________|
 
 var schemaCalc = {
+
   //an schema object used to validate future operation objects.
   schema: {
     name: {value: "name of operation"},
     numArgs: {value: 1},
     operation: {value: function(){console.log("I'm the default operation")}}
   },
+
   //operations array to store the operation objects.
   operations: [],
 
-//**FUNCTIONS**
-
-//| addTo is a function that match new objects against   |
-//| the schema & add them to operations if they're valid |
-//| returning a success/failure msg.                     |
-//|______________________________________________________|
+                    //**FUNCTIONS**
+  //|______________________________________________________|
+  //| addTo is a function that match new objects against   |
+  //| the schema & add them to operations if they're valid |
+  //| returning a success/failure msg.                     |
+  //|______________________________________________________|
 
   addTo: function(obj){
     var myObject = {};
-    //the method 'addTo' uses schema objects to validate new objects.
-    //we search for properties inside the object passed.
+    //the method 'addTo' uses schema objects to validate new objects:
+    //it searches for properties inside the object passed,
     for (key in obj) {
     	var opProperty = obj[key];
-      //is the property is found inside the schema.
+      //if the property is found inside the schema
       if (key in this.schema){
-        //and matches with the an schema's property.
+        //and matches with a schema's property
         if (typeof(opProperty.value) === typeof(this.schema[key].value)) {
           //adds the property to the temporal object.
           myObject[key] = obj[key];
@@ -42,7 +45,7 @@ var schemaCalc = {
           console.log("Property".toUpperCase, key, "isn't in schema".toUpperCase());
           return obj;
        }
-    }//if everything went ok and property 'name'&'operation' are included.
+    }//if everything went ok and property 'name'&'operation' are included
     if ('name' in obj && 'operation' in obj){
       //we add the new object to the collection of operations.
       this.operations.push(myObject);
@@ -55,11 +58,11 @@ var schemaCalc = {
     }
   },
 
-
-  //| operate is a function that searches an operation     |
-  //| passed as arg inside operations array and operate the|
-  //| function inside with the number passed as args       |
-  //|______________________________________________________|
+  //|____________________________________________________|
+  //| operate is a function that searches an operation   |
+  //| passed as arg in operations array and operates the |
+  //| function inside with the numbers passed as args.   |
+  //|____________________________________________________|
 
   operate: function(op, a, b, c){
     //we search for all the op. objects inside the array
@@ -73,7 +76,8 @@ var schemaCalc = {
     console.log("Operation not found");
     return undefined;
   },
-
+  
+  //|_______________________________________________|
   //| changeSchema is a function that reset the     |
   //| schema with the object passed as an argument  |
   //|_______________________________________________|
@@ -91,4 +95,4 @@ example: {
     name: {value: "sum", does: "Add up to 3 numbers"},
     numArgs: {value: 3, does: "Give the numbers of args that operation takes"},
     operation: {value: function(a, b, c = 0){return a + b + c}}
-  },
+  };
