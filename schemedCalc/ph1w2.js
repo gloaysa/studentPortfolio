e5ea73db22fae62746bcf4f95379b50f99550573
Guillermo Loaysa <gloaysa@gmail.com>
2017-04-26 00:43:03 +0200

    schemaCalc finished

--- schemedCalc/ph1w2.js
+++ schemedCalc/ph1w2.js
@@ -5,14 +5,14 @@
 //|________________________________________________|
 
 var schemaCalc = {
-  //an schema object used to validate future operation objects
+  //an schema object used to validate future operation objects.
   schema: {
-    name: {type: "string", fallback: "I'm the default name"},
-    numArgs: {type: "number", fallback: 1},
-    operation: {type: "function", fallback: function(){console.log("I'm the default operation")}}
+    name: {value: "name of operation"},
+    numArgs: {value: 1},
+    operation: {value: function(){console.log("I'm the default operation")}}
   },
-  //operation array to store the operations
-  operation: [],
+  //operations array to store the operation objects.
+  operations: [],
 
 //**FUNCTIONS**
 
@@ -21,41 +21,74 @@ var schemaCalc = {
 //| returning a success/failure msg.                     |
 //|______________________________________________________|
 
-  addTo: function(op){
+  addTo: function(obj){
     var myObject = {};
-    //the method 'addTo' uses schema objects to validate new objects
-    for (key in op) {
-    	var llave = op[key];
+    //the method 'addTo' uses schema objects to validate new objects.
+    //we search for properties inside the object passed.
+    for (key in obj) {
+    	var opProperty = obj[key];
+      //is the property is found inside the schema.
       if (key in this.schema){
-        if (llave.type === this.schema[key].type) {
-          myObject[key] = op[key];
-          addToCollection = true;
+        //and matches with the an schema's property.
+        if (typeof(opProperty.value) === typeof(this.schema[key].value)) {
+          //adds the property to the temporal object.
+          myObject[key] = obj[key];
         } else {
-          myObject[key] = this.schema[key]
-          console.log(llave.type, "isn't in the schema. Default value added".toUpperCase());
+          console.log("Value of".toUpperCase(), opProperty.value, "doesn't match.".toUpperCase());
+          return obj;
         }
       } else {
-          addToCollection = false;
           myObject = {};
-          console.log(key, " isn't in schema".toUpperCase());
-          return op;
+          console.log("Property".toUpperCase, key, "isn't in schema".toUpperCase());
+          return obj;
        }
-    }
-    if ('name' in op && 'operation' in op){
-      if (addToCollection === true){
-        this.operation.push(myObject);
-        console.log("New object added to operations ".toUpperCase(), this.operation);
-        return myObject
-      } else {
-        return op
-      }
-    }
+    }//if everything went ok and property 'name'&'operation' are included.
+    if ('name' in obj && 'operation' in obj){
+      //we add the new object to the collection of operations.
+      this.operations.push(myObject);
+      console.log("New object added to operations ".toUpperCase());
+      return this.operations;
+    } else {
       myObject = {};
       console.log("Property 'name' and 'operation' are required".toUpperCase());
-      return op
+      return obj;
+    }
   },
 
-  //a method 'operate' that searches for an operation with the name passed in
-  //contains a string indicating operation to be used and numbers to be operated on
 
-}
+  //| operate is a function that searches an operation     |
+  //| passed as arg inside operations array and operate the|
+  //| function inside with the number passed as args       |
+  //|______________________________________________________|
+
+  operate: function(op, a, b, c){
+    //we search for all the op. objects inside the array
+    for (x in this.operations){
+      var loop = this.operations[x];
+      //we search for a match between op and object names
+      if (op === loop.name.value){
+        return loop.operation.value(a, b, c);
+      }
+    }//if we don't find any, we say so and return undefined.
+    console.log("Operation not found");
+    return undefined;
+  },
+
+  //| changeSchema is a function that reset the     |
+  //| schema with the object passed as an argument  |
+  //|_______________________________________________|
+
+    changeSchema: function(obj){
+      this.schema = obj;
+      console.log("The schema has been modified");
+      return this.schema;
+    }
+
+};
+
+//TEST SCRIPT:
+example: {
+    name: {value: "sum", does: "Add up to 3 numbers"},
+    numArgs: {value: 3, does: "Give the numbers of args that operation takes"},
+    operation: {value: function(a, b, c = 0){return a + b + c}}
+  },
